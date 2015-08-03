eeb9c23fefa8b97491c0d82ac277f80ea8c72325
AlexOlleta <cstinos2@gmail.com>
2015-08-03 17:43:15 +0200

    modulo 8
    subiendo cambios para edición y control de errores

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,13 +1,69 @@
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req,res,next,quizId){
+	models.Quiz.find(quizId).then(function(quiz){
+		if(quiz){req.quiz = quiz; next();}
+		else{ next(new Error('No existe quizId = ' + quizId));}
+	}).catch(function(error){next(error);});
+};
+
+// GET /quizes
+exports.index = function(req,res){
+	module.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs',{quizes:quizes, errors: []});
+	}).catch(function(error){next(error);});
+};
+
 //GET /quizes/question
-exports.question = function(req,res){
-	res.render('quizes/question',{pregunta: '¿Capital de Italia?'});
+exports.show = function(req,res){
+	res.render('quizes/show',{quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/answer
 exports.answer = function(req,res){
-	if(req.query.p1.replace(/r/,'R') === 'Roma'){
-		res.render('quizes/answer',{respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer',{respuesta: 'Incorrecto'});
-	}
+	var resultado = 'Incorrecto';
+	if(req.query.p1.replace(/r/,'R') === quiz.respuesta){resultado = 'Correcto';}
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado, errors: []});
 };
+
+//GET /quizes/new
+exports.new = function(reg,res){
+	var quiz = models.Quiz.build({pregunta: 'Pregunta', respuesta: 'Respuesta', tema: 'Tema'});
+	res.render('quizes/new', {quiz:quiz, errors: []});
+};
+
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//guarda en DB los campos pregunta y respuesta de quiz
+	quiz
+	.validate().then(function(err){if (err){
+		res.render('quizes/new', {quiz: quiz,error: err.errors});
+	} else {
+		quiz.save({fields: ["pregunta","respuesta","tema"]}).then(function(){
+		res.redirect('/quizes');});
+	}});
+};
+
+//PUT /quizes/update
+exports.update=function(req,res){
+	req.quiz.pregunta=req.body.quiz.pregunta;
+	req.quiz.respuesta=req.body.quiz.respuesta;
+	req.quiz.tema=req.body.quiz.tema;
+	req.quiz.validate().then(function(err){if(err){
+		res.render('quizes/edit',{quiz:req.quiz,errors:err.errors});
+	}else{
+		req.quiz.save({fields:["pregunta","respuesta","tema"]}).then(function(){
+			res.redirect('/quizes');});
+		}
+	});
+};
+
+//DELETE /quizes/destroy
+exports.destroy=function(req,res){
+	req.quiz.destroy().then(function(){res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
+
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,23 @@ var quizController = require('../controllers/quiz_controllers');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Cuestionario Cultural' });
+  res.render('index', { title: 'Cuestionario Cultural', errors: []});
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId',quizController.load); //autoload :quizId
+
+// Definicion de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
+router.put('/quizes/:quizId(\\d+)',quizController.update);
+router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
 router.get('/author',function(req,res){
-	res.render('author',{title: 'Cuestionario Cultural' });
+	res.render('author',{title: 'Cuestionario Cultural', errors: []});
 });
 
 module.exports = router;
